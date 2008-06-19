bd7cd4509eb09a0b0e183df5f67dae67c3fc6fa8
Bahadır Kandemir <bahadir@pardus.org.tr>
2008-06-19 09:06:48 +0000

    remove gnome policies

--- test_pksimple.py
+++ test_pksimple.py
@@ -1,7 +1,7 @@
 import polkit
 import os
 
-action_ids = ("org.freedesktop.hal.device-access.camera", "org.gnome.clockapplet.mechanism.configurehwclock")
+action_ids = ("org.freedesktop.hal.device-access.camera", "org.freedesktop.hal.storage.mount-fixed")
 
 for a in action_ids:
     print a, polkit.check_auth(os.getpid(), a)
@@ -14,4 +14,4 @@ print "= " * 20
 print "org.freedesktop.hal.device-access.camera", polkit.check_auth(os.getpid(), "org.freedesktop.hal.device-access.camera")
 print "= " * 20
 
-print polkit.auth_obtain("org.gnome.clockapplet.mechanism.configurehwclock", 0, os.getpid())
+print polkit.auth_obtain("org.freedesktop.hal.storage.mount-fixed", 0, os.getpid())
